0b6c1abdda31b101bca30ca339cfac7e2b036112
Mike Casaday <mike.casaday@gmail.com>
2019-03-18 12:45:57 -0700

    Port setup.sh to a Windows batch file

---
+++ setup.cmd
@@ -0,0 +1,30 @@
+@echo off
+set TUTORIAL_HOME=%USERPROFILE%\src\djangogirls
+
+set /p github_username="What's your GitHub username? "
+set /p github_email="What's your GitHub email? "
+
+md %TUTORIAL_HOME%
+cd %TUTORIAL_HOME% || exit /b
+
+:: Start docker if it's not already running
+docker-machine start
+
+:: Create symlink inside the docker machine so capital drive letters will work in mount paths
+docker-machine ssh default sudo ln -sf /c /C
+
+:: Store current working directory in env var
+set current_dir=%cd%
+
+:: Convert backslashes to forward-slashes
+set current_dir=%current_dir:\=/%
+
+:: Remove ":"
+set current_dir=/%current_dir::=%
+
+docker run --rm -v %current_dir%:/root/src/djangogirls gsong/djangogirls-app git clone https://github.com/%github_username%/my-first-blog.git .
+
+(
+  echo GITHUB_USERNAME=%github_username%
+  echo GITHUB_EMAIL=%github_email%
+) > .env
